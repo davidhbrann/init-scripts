14ceb1a7ae23e45784cb021c5d4fda83ace67f91
David Brann <david_brann@g.harvard.edu>
2020-04-14 18:59:34 -0400

    add o2 scripts

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ ln -sfv ${DIR}/.aliases ~/.aliases
 ln -sfv ${DIR}/.env_vars ~/.env_vars
 ln -sfv ${DIR}/ssh/config ~/.ssh/config
 ln -sfv ${DIR}/.source_dotfiles ~/.source_dotfiles
-# add o2 scripts to path
-export PATH=$PATH:${DIR}/o2
+mkdir -p ~/.local/bin
+ln -sfv ${DIR}/o2/* ~/.local/bin/
+ln -sfv ${DIR}/.add_to_path ~/.add_to_path
