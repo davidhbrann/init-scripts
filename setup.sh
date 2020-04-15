b90e321cf821b3ad0622e4ecac3a0718c0eade27
David Brann <david_brann@g.harvard.edu>
2020-04-14 21:31:43 -0400

    add option to link ssh config

--- setup.sh
+++ setup.sh
@@ -1,10 +1,28 @@
+#!/bin/bash
+OPTIND=1
 DIR=$(pwd);
+SSH=""
+
+while getopts 's' flag; do
+  case "${flag}" in
+    s) SSH=true;;
+    *) echo "unkown args">&2
+    exit 1
+    ;;
+  esac
+done
+
+echo "Linking files"
 ln -sfv ${DIR}/.vimrc ~/.vimrc
 ln -sfv ${DIR}/.tmux.conf ~/.tmux.conf
 ln -sfv ${DIR}/.aliases ~/.aliases
 ln -sfv ${DIR}/.env_vars ~/.env_vars
-ln -sfv ${DIR}/ssh/config ~/.ssh/config
 ln -sfv ${DIR}/.source_dotfiles ~/.source_dotfiles
 mkdir -p ~/.local/bin
 ln -sfv ${DIR}/o2/* ~/.local/bin/
 ln -sfv ${DIR}/.add_to_path ~/.add_to_path
+
+if [ $SSH ]; then
+    echo "linking ssh";
+    ln -sfv ${DIR}/ssh/config ~/.ssh/config
+fi
